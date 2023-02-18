63d8306439d2f9345b5837bf7e98b8fb5e07b0e9
kirikirimai <go.revolution.no3@gmail.com>
2023-02-18 23:33:03 +0900

    read jsondata

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
 import ReadData from "./pages/ReadData";
 
 function App() {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path={"/"} element={<Home />} />
-        <Route path={"/readdata"} element={<ReadData />} />
+        <Route path={"/"} element={<ReadData />} />
       </Routes>
     </BrowserRouter>
   )
